1c1802ce7b3e0efd2424b189077cfa878d34bdce
JakeMikiewicz <78130760+JakeMikiewicz@users.noreply.github.com>
2022-11-04 21:16:11 -0500

    Add files via upload

---
+++ Milkshake.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Milkshake {
+
+	public void order(String size) {
+		if (size.contentEquals("small") || size.contentEquals("medium") || size.contentEquals("large") || size.contentEquals("x-large")) {
+			System.out.println("Ordering " + size + " milkshake...");
+		} else if (size.contentEquals("exit")) {
+			System.out.println("Thank you for visiting!");
+			Program.SleepXSeconds(2);
+			System.exit(0);
+		} else {
+			System.out.println("Sorry, invalid option. Please try again.");
+			System.out.println("Restarting store...");
+			Program.SleepXSeconds(2);
+			MilkshakeShop.main(null);
+		}
+	}
+
+	public void SmallMilkshake() {
+        System.out.println("Small milkshake ordered!");
+    }
+    public void MediumMilkshake() {
+        System.out.println("Medium milkshake ordered!");
+    }
+    public void LargeMilkshake() {
+        System.out.println("Large milkshake ordered!");
+    }
+    public void XLargeMilkshake() {
+        System.out.println("X-Large milkshake ordered!");
+    }
+}
+
---
+++ MilkshakeShop.java
@@ -0,0 +1,84 @@
+// Milkshake Project
+// Jake Mikiewicz
+// Due in December 2022
+
+import java.util.Scanner;
+import util;
+import util.Milkshake;
+import util.AssertNotNullTest;
+
+class Program {
+    public static void SleepXSeconds(int seconds) {
+        seconds = seconds * 1000; // number of milliseconds to sleep
+        long start = System.currentTimeMillis();
+        while(start >= System.currentTimeMillis() - seconds);
+    }
+
+    public static void clearScreen() {   
+        System.out.print("\033[H\033[2J");   
+        System.out.flush();   
+    } 
+}
+
+public class MilkshakeShop {
+
+    private static MilkshakeShop instance = null;
+ 
+    public static MilkshakeShop getInstance() {
+        if (instance == null) {
+            instance = new MilkshakeShop();
+        }
+        return instance;
+    }
+ 
+    public MilkshakeShop orderMilkshake(String size) {
+        Milkshake order = new Milkshake();
+        if (size.contentEquals("small")) {
+            order.SmallMilkshake();
+
+        } else if (size.contentEquals("medium")) {
+            order.MediumMilkshake();
+ 
+        } else if (size.contentEquals("large")) {
+            order.LargeMilkshake();
+
+        } else if (size.contentEquals("x-large")) { 
+            order.XLargeMilkshake();
+
+        } else {
+            return null;
+        }
+        return null;
+    }
+
+    public static void main(String args[])
+    {
+        Program.clearScreen();
+        Program.SleepXSeconds(1);
+
+        System.out.println("Welcome to Jake's Milkshake Shop!");
+        Program.SleepXSeconds(2);
+
+        System.out.println("Please select an option to order a milkshake.");
+        System.out.println("You may enter 'exit' to quit the store. ");
+        System.out.print("Type small, medium, large, or x-large: ");
+        Scanner sc = new Scanner(System.in);    //System.in is a standard input stream  
+        String size = sc.next();
+
+        MilkshakeShop shop = new MilkshakeShop();
+        Milkshake drink = new Milkshake();
+        
+
+        // ordering sticker
+        drink.order(size);
+        Program.SleepXSeconds(2);
+        Program.clearScreen();
+        shop.orderMilkshake(size);
+
+        System.out.println("Enjoy that creamy decliciousness!");
+
+        // close scanner for cleanliness
+        sc.close();
+        
+    }
+}
